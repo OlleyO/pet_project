474046c6c297fc5445096ee9666fc400f01f55db
Oleh <my.mail.olech@gmail.com>
2022-08-09 19:44:13 +0300

    feat: mock data simulating decoded json

---
+++ lib/mocks/data.dart
@@ -0,0 +1,81 @@
+final posts = [
+  {
+    "id": "12345",
+    "first_name": "Nettie",
+    "last_name": "Fernandez",
+    "created_at": "2022-08-08 18:47:51.416",
+    "photo_url": "assets/images/post_image.png",
+    "author_photo_url": "assets/images/img.png",
+    "description":
+        """TB to New York October 2018.\n"You shouldn't wait for other people to make special things happen. You have to create your own memories." Heidi Klum""",
+    "likes": 439,
+    "comments": 34,
+  },
+  {
+    "id": "12346",
+    "first_name": "Tommy",
+    "last_name": "Redl",
+    "created_at": "2021-07-22 15:21:51.416",
+    "photo_url": "assets/images/post_image.png",
+    "author_photo_url": "assets/images/img_7.png",
+    "description":
+        """If she wants you to deny, there's always her mother to try""",
+    "likes": 102,
+    "comments": 10,
+  }
+];
+
+final creators = [
+  {
+    "id": "12345",
+    "photo_url": "assets/images/img_1.png",
+  },
+  {
+    "id": "12346",
+    "photo_url": "assets/images/img_2.png",
+  },
+  {
+    "id": "12347",
+    "photo_url": "assets/images/img_3.png",
+  },
+  {
+    "id": "12348",
+    "photo_url": "assets/images/img_4.png",
+  },
+  {
+    "id": "12349",
+    "photo_url": "assets/images/img_5.png",
+  },
+  {
+    "id": "12350",
+    "photo_url": "assets/images/img_6.png",
+  },
+  {
+    "id": "12351",
+    "photo_url": "assets/images/img_7.png",
+  },
+  {
+    "id": "12352",
+    "photo_url": "assets/images/img_2.png",
+  },
+  {
+    "id": "12353",
+    "photo_url": "assets/images/img_1.png",
+  },
+  {
+    "id": "12354",
+    "photo_url": "assets/images/Phone/Rectangle 50.png",
+  },
+];
+
+final userInfo = {
+  "id": "1234567",
+  "followers": 110000,
+  "following": 448,
+  "first_name": "Alexander",
+  "last_name": "Sokolov",
+  "job_title": "photographer",
+  "description":
+      "Like to travel and shoot cinematic and b/w photos\nTools - Capture One for Raw",
+  "photo_url": "assets/images/img.png",
+};
