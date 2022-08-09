791688f481d29be49284002ea1a52360f31efd40
Oleh <my.mail.olech@gmail.com>
2022-08-09 19:42:54 +0300

    feat: app icons generated by icomoon app

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const String _fontFamily = 'Icons';
+
+  static const IconData arrow_right = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData comments = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData heart = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData menu = IconData(0xe905, fontFamily: _fontFamily);
+}
