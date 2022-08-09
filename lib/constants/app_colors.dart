d4f1ffa40e3ddf8abb27d6e692095c6c8b188638
Oleh <my.mail.olech@gmail.com>
2022-08-09 19:42:03 +0300

    feat: app colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static final Color black = Color(0xFF010101);
+  static final Color gray = Color(0xFFc9cad1);
+  static final Color blue = Color(0xFF0087ff);
+  static final Color darkGray = Color(0xFF808080);
+  static final Color red = Color(0xFFfb766e);
+  static final Color green = Color(0xFF5eec70);
+  static final Color lightGray = Color(0xFFfafbfc);
+}
